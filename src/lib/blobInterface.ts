3fe0ea293b7e71f4ba3adb2c64efb5c8b852f573
jibhong <pisitjibhong@gmail.com>
2025-10-01 13:20:28 +0700

    Fix NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET undefined in client 2

--- src/lib/blobInterface.ts
+++ src/lib/blobInterface.ts
@@ -7,9 +7,9 @@
 //     const res_json = await res.json();
 //     return res_json.url;
 // }
-const BUCKET = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET; 
 
 export async function getBlobUrl(path: string): Promise<string> {
+  const BUCKET = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET; 
   if (path.startsWith("/")) path = path.slice(1);
   const encodedPath = encodeURIComponent(path);
   return `https://firebasestorage.googleapis.com/v0/b/${BUCKET}/o/${encodedPath}?alt=media`;
