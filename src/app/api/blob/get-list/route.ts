b5622a9ac7ca8c6e90e721fcf09e560ce57c8857
jibhong <pisitjibhong@gmail.com>
2025-10-01 12:17:35 +0700

    Fix vercel error 9

--- src/app/api/blob/get-list/route.ts
+++ src/app/api/blob/get-list/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { verifyTokenServer } from "@/lib/tokenAuth.server";
-import { getAllBlogPath, listFolderPaths } from "@/lib/firebaseInterface";
+import { getAllBlogPath } from "@/lib/firebaseInterface";
 import { getProtectedFilesUrls } from "@/lib/firebaseInterface";
 import path from "path";
-
 
 export async function GET(req: Request) {
   const cookieHeader = (await cookies()).get("session")?.value;
@@ -13,17 +12,14 @@ export async function GET(req: Request) {
   if (!ok) {
     return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
   }
-  
 
-  const allPath = await getAllBlogPath();
+  const allPath = (await getAllBlogPath()) ?? [];
   const output: Record<string, { name: string; url: string }[]> = {};
 
-
-  for (const folder of allPath){
-    output[path.basename(folder)] = await getProtectedFilesUrls(folder);
+  for (const folder of allPath) {
+    const urls = (await getProtectedFilesUrls(folder)) ?? [];
+    output[path.basename(folder)] = urls;
   }
-
-  // console.log(output)
 
   return NextResponse.json(output ?? {}, { status: 200 });
 }
