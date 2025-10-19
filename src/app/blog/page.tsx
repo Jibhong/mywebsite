e01ed9fe23743520c5ba13a6fbd8d795f473471e
jibhong <pisitjibhong@gmail.com>
2025-10-19 22:52:15 +0700

    Added skeleton loading to card

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -40,14 +40,19 @@ function HomeContent() {
       setCardList(list)
       console.log(list)
       const tempCards : Card[] = [];
-      for(let i=0; i<Math.min(6,list.length); i++){
+      setCards(Array(list.length).fill(null));
+      for(let i=0; i<list.length; i++){
         console.log(i);
         const res = await fetch(await getBlobUrl("/blog_page/"+list[i]+"/metadata.json"));
         const card = await res.json(); 
 
         tempCards.push({ ...card, path: list[i], thumbnail: getBlobUrl("/blog_page/"+list[i]+"/preview.webp"), link: "blog/"+list[i]}); // append multiple times to temp array
-        setCards(prev => [...prev, { ...card, path: list[i], thumbnail: getBlobUrl("/blog_page/"+list[i]+"/preview.webp"), link: "blog/"+list[i]}]);
-
+        // setCards(prev => [...prev, { ...card, path: list[i], thumbnail: getBlobUrl("/blog_page/"+list[i]+"/preview.webp"), link: "blog/"+list[i]}]);
+        setCards(prev => {
+          const next = [...prev];
+          next[i] = { ...card, path: list[i], thumbnail: getBlobUrl("/blog_page/" + list[i] + "/preview.webp"), link: "blog/" + list[i] };
+          return next;
+        });
       }
       setAllCard(tempCards);
       if (searchQuery) {
@@ -105,29 +110,47 @@ function HomeContent() {
           <div className="">
             <div id="card_container" className="px-[10%] portrait:px-[5%] max-w-[1600px] mx-auto grid grid-cols-1 gap-8">
                 {cards.length > 0 ? (
-                  cards.map((data,index) => (
-                    <Link href={data.link} key={index} className="flex justify-between card-background gap-4 ">
-                      <div className="flex flex-col h-full">
-                        <h2 className="line-clamp-2 sm:line-clamp-1 text-xl font-bold mb-1 text-gray-700">{data.title}</h2>
-                        <p className="line-clamp-3 xl:line-clamp-5 text-gray-700 mb-1">{data.description}</p>
-                        <p className="text-xs italic text-gray-700 mt-auto">
-                          {new Date(data.date * 1000).toLocaleDateString("en-US", {
-                            month: "short",
-                            day: "2-digit",
-                            year: "numeric",
-                          })}
-                        </p>
-                      </div>
+                  cards.map((data, index) => 
+                    <div key={index} className="relative animate-card-in">
+                      <Link href={data?.link ?? "#"} className="flex justify-between card-background gap-4 transition-opacity duration-200">
+                        <div className="flex flex-col h-full">
+                          <h2 className="line-clamp-2 sm:line-clamp-1 text-xl font-bold mb-1 text-gray-700">{data?.title ?? ""}</h2>
+                          <p className="line-clamp-3 xl:line-clamp-5 text-gray-700 mb-1">{data?.description ?? ""}</p>
+                          <p className="text-xs italic text-gray-700 mt-auto">
+                            {new Date(data?.date ?? 0 * 1000).toLocaleDateString("en-US", {
+                              month: "short",
+                              day: "2-digit",
+                              year: "numeric",
+                            })}
+                          </p>
+                        </div>
+                      
+                        <Image 
+                            src={data?.thumbnail ?? "/loading.gif"}
+                            alt="Repo image" 
+                            width={400} 
+                            height={400} 
+                            className="w-30 h-30 xl:w-40 xl:h-40 object-cover rounded-2xl justify-self-end"
+                        />
+                      </Link>
+                      <div className={
+                        "absolute inset-0 flex justify-between gap-4 card-background transition-opacity duration-400 ease-out " +
+                        (data === null ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none")
+                      }>
+                        <div className="flex-1 flex flex-col h-full">
+                          <div className="h-6 w-3/4 rounded-md bg-gray-200 mb-4" />
+                          <div className="h-4 w-9/10 rounded-md bg-gray-200 mb-2" />
+                          <div className="h-4 w-[calc(100%-5)] rounded-md bg-gray-200 mb-2" />
+                          <div className="h-4 w-[calc(100%-1rem)] rounded-md bg-gray-200 mb-2" />
+
+                          <div className="h-4 w-20 rounded-md bg-gray-200 mt-auto" />
+                        </div>
                     
-                      <Image 
-                          src={data.thumbnail}
-                          alt="Repo image" 
-                          width={400} 
-                          height={400} 
-                          className="w-30 h-30 xl:w-40 xl:h-40 object-cover rounded-2xl justify-self-end"
-                      />
-                    </Link>
-                  ))
+                      <Image src={"/loading.gif"} alt="Repo image" width={400} height={400} className="w-30 h-30 xl:w-40 xl:h-40 object-cover rounded-2xl justify-self-end" />
+                      
+                      </div>
+                    </div>
+                  )
                 ) : (
                   <p className="col-span-full text-center text-gray-500 text-lg">
                     No results found
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Suspense } from "react";
 import { getBlobUrl } from "@/lib/blobInterface";
 
 
-const BLOB  = process.env.NEXT_PUBLIC_VERCEL_BLOB_URL;
+const BLOB = process.env.NEXT_PUBLIC_VERCEL_BLOB_URL;
 
 interface Card {
   title: string;
@@ -19,32 +19,36 @@ interface Card {
 }
 
 export default function Home() {
-  
 
   const [cards, setCards] = useState<Card[]>([]);
 
 
   useEffect(() => {
-    
+
     async function fetchCards() {
       const res = await fetch(await getBlobUrl("/blog_page/index.json"));
       const card_list = await res.json();
       console.log(card_list);
-      const tempCards : Card[] = [];
-      for(let i=0; i<Math.min(6,card_list.length); i++){
+      const tempCards: Card[] = [];
+      setCards(Array(Math.min(6, card_list.length)).fill(null));
+      for (let i = 0; i < Math.min(6, card_list.length); i++) {
         console.log(i);
-        const res = await fetch(await getBlobUrl("/blog_page/"+card_list[i]+"/metadata.json"));
-        const card = await res.json(); 
-        console.log(card);      
-        tempCards.push({ ...card, path: card_list[i], thumbnail: await getBlobUrl("/blog_page/"+card_list[i]+"/preview.webp"), link: "blog/"+card_list[i]}); // append multiple times to temp array
-        setCards(prev => [...prev, { ...card, path: card_list[i], thumbnail: getBlobUrl("/blog_page/"+card_list[i]+"/preview.webp"), link: "blog/"+card_list[i]}]);
- 
+        const res = await fetch(await getBlobUrl("/blog_page/" + card_list[i] + "/metadata.json"));
+        const card = await res.json();
+        console.log(card);
+        tempCards.push({ ...card, path: card_list[i], thumbnail: getBlobUrl("/blog_page/" + card_list[i] + "/preview.webp"), link: "blog/" + card_list[i] }); // append multiple times to temp array
+        setCards(prev => {
+          const next = [...prev];
+          next[i] = { ...card, path: card_list[i], thumbnail: getBlobUrl("/blog_page/" + card_list[i] + "/preview.webp"), link: "blog/" + card_list[i] };
+          return next;
+        });
+
       }
       // setCards(tempCards);
     }
     fetchCards();
   }, []);
-  
+
   return (
     <div className="font-sans min-h-screen bg-orange-50 pt-30 overflow-y-auto ">
       <div className="min-h-screen pb-10">
@@ -53,10 +57,10 @@ export default function Home() {
 
         <div className=" px-10 flex flex-col items-center mb-16">
           <div className="w-40 h-40 relative rounded-full overflow-hidden border-4 border-white shadow-lg">
-            <Image 
-              src="/profile.png" 
-              alt="User Profile" 
-              fill 
+            <Image
+              src="/profile.png"
+              alt="User Profile"
+              fill
               className="object-cover"
             />
           </div>
@@ -72,16 +76,16 @@ export default function Home() {
           <p className="mt-4 text-center text-gray-700 max-w-xl text-lg sm:text-xl">
             {"I'm a self-taught developer and creative enthusiast. I love coding, 3D modeling, VFX, and exploring new technologies."}
           </p>
-        
+
           <div className="mt-6 flex space-x-4">
-            <a 
-              href="https://github.com/Jibhong" 
+            <a
+              href="https://github.com/Jibhong"
               className="px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700 transition"
             >
               GitHub
             </a>
-            <a 
-              href="https://www.linkedin.com/in/Jibhong/" 
+            <a
+              href="https://www.linkedin.com/in/Jibhong/"
               className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-500 transition"
             >
               LinkedIn
@@ -99,22 +103,36 @@ export default function Home() {
 
         <div id="card_container" className="px-[10%] portrait:px-[5%] max-w-[2000px] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
           {cards.length > 0 ? (
-              cards.map((data,index) => (
-                <Link href={data.link} key={index} className="flex justify-between  card-background gap-4 ">
+            cards.map((data, index) => 
+              <div key={index} className="relative animate-card-in">
+                <Link href={data?.link ?? "#"} key={index} className="flex justify-between  card-background gap-4 transition-opacity duration-200">
                   <div>
-                    <h2 className="line-clamp-2 sm:line-clamp-1 text-xl font-bold mb-2 text-gray-700">{data.title}</h2>
-                    <p className="line-clamp-3 xl:line-clamp-5 text-gray-700 mb-2">{data.description}</p>
+                    <h2 className="line-clamp-2 sm:line-clamp-1 text-xl font-bold mb-2 text-gray-700">{data?.title ?? ""}</h2>
+                    <p className="line-clamp-3 xl:line-clamp-5 text-gray-700 mb-2">{data?.description ?? ""}</p>
                   </div>
-                
-                  <Image 
-                      src={data.thumbnail}
-                      alt="Repo image" 
-                      width={400} 
-                      height={400} 
-                      className="w-30 h-30 xl:w-40 xl:h-40 object-cover rounded-2xl justify-self-end"
+
+                  <Image
+                    src={data?.thumbnail ?? "/loading.gif"}
+                    alt="Repo image"
+                    width={400}
+                    height={400}
+                    className="w-30 h-30 xl:w-40 xl:h-40 object-cover rounded-2xl justify-self-end"
                   />
                 </Link>
-              ))
+                <div className={
+                  "absolute inset-0 flex justify-between gap-4 card-background transition-opacity duration-400 ease-out " +
+                  (data === null ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none")
+                }>
+                  <div className="flex-1">
+                    <div className="h-6 w-3/4 rounded-md bg-gray-200 mb-4" />
+                    <div className="h-4 w-9/10 rounded-md bg-gray-200 mb-2" />
+                    <div className="h-4 w-[calc(100%-5)] rounded-md bg-gray-200 mb-2" />
+                    <div className="h-4 w-[calc(100%-1rem)] rounded-md bg-gray-200 mb-2" />
+                  </div>
+                  <Image src={"/loading.gif"} alt="Repo image" width={400} height={400} className="w-30 h-30 xl:w-40 xl:h-40 object-cover rounded-2xl justify-self-end" />
+                </div>
+              </div>
+            )
           ) : (
             <p className="col-span-full text-center text-gray-500 text-lg">
               No results found
@@ -122,7 +140,7 @@ export default function Home() {
           )
           }
         </div>
-        
+
 
 
 
